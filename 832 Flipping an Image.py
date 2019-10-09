9d88556c5f55206faf078b97dbf6abe9286b26e9
Felzj <54878400+jeffy34931@users.noreply.github.com>
2019-10-09 23:32:41 +0700

    Add files via upload

---
+++ 832 Flipping an Image.py
@@ -0,0 +1,15 @@
+"""Can only run in Leetcode"""
+"""https://leetcode.com/problems/flipping-an-image/"""
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        result = []
+        count = 0
+        for i in A:
+            result.append([])
+            for j in i[::-1]:
+                if j == 0:
+                    result[count].append(1)
+                else:
+                    result[count].append(0)
+            count += 1
+        return result
